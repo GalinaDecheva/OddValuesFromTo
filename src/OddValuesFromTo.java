65ef3a0ddab9def8d8846d842c46a8e7ce33b081
Galina Decheva <de_galina@abv.bg>
2017-02-26 23:10:24 +0200

    adding =

--- src/OddValuesFromTo.java
+++ src/OddValuesFromTo.java
@@ -5,7 +5,7 @@ public class OddValuesFromTo {
 		// TODO Auto-generated method stub
 		Scanner input = new Scanner(System.in);
 		
-		for(int i = -10; i < 10; i++){
+		for(int i = -10; i <= 10; i++){
 			if (i % 2 == 1 || i % 2 == -1){
 				System.out.println(i);
 			}
