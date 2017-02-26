51d02ebbaef45e033a0a7a3c6b7d55a2000a0609
Galina Decheva <de_galina@abv.bg>
2017-02-26 23:06:40 +0200

    Ready

---
+++ src/OddValuesFromTo.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class OddValuesFromTo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+		
+		for(int i = -10; i < 10; i++){
+			if (i % 2 == 1 || i % 2 == -1){
+				System.out.println(i);
+			}
+				
+		}
+	input.close();
+	}
+	
+}
